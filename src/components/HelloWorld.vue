ca15f3fdcb05506a1bf76982abae51416a1b1f21
emmyisezerano8 <emmyisezerano8@gmail.com>
2025-11-29 19:40:55 +0000

    gh repo clone emmyisezerano8/Agasobanuyeshow

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,12 +4,15 @@
       <img src="https://www.google.com/imgres?q=agasobanuyeshow&imgurl=https%3A%2F%2Fyt3.googleusercontent.com%2Ffn7Z84nnl4RhFnswzU92faANJ3VMPvpOkQ8DWp3mF-a2SF-ziAXs--dGIFqZnlnY1jHxanRJiQ%3Ds900-c-k-c0x00ffffff-no-rj&imgrefurl=https%3A%2F%2Fwww.youtube.com%2F%40agasobanuyeshow-h4o&docid=79gNj8l5WB1yMM&tbnid=H9IAwfcseCaRGM&vet=12ahUKEwi72cuunKiPAxV_dUEAHahhGe8QM3oECGYQAA..i&w=900&h=900&hcb=2&itg=1&ved=2ahUKEwi72cuunKiPAxV_dUEAHahhGe8QM3oECGYQAA" alt="Agasobanuyeshow Logo" class="logo" />
       <h1>Agasobanuyeshow</h1>
     </header>
-  <nav1>
+  <nav1> 
+    <button @click="open = '<a href="Home" target="_blank">Home</a>'">Home</button>
+   </nav1>
+  <nav2>
     
       <button @click="view = 'firms'">Firms</button>
       <button @click="view = 'login'">Login</button>
       <button @click="view = 'signup'">Sign Up</button>
-       </nav1>
+       </nav2>
     
          <section v-if="view === 'firms'">
       <h2>Firms</h2>
@@ -19,6 +22,7 @@
           <iframe width="300" height="200" 
                   src="https://www.youtube.com/embed/6iFcGNOWnA8" 
                   title="Hancock Trailer" frameborder="0" allowfullscreen></iframe>
+                  
         </li>
         <li>
           Death Note
@@ -90,6 +94,8 @@
 </template>
 
 <script>
+import { openBlock } from 'vue';
+
 export default {
   name: "HelloWorld",
   data() {
